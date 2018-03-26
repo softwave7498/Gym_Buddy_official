f451baa598bc149a23ac3c1b761de9a1273c0c64
MOHIT KUMAR CHUG <chug.mohit7@gmail.com>
2018-03-26 08:47:02 +0530

    Gym buddy code upload for playstore

--- Gym_Buddy2/app/src/main/java/com/dscudr/gym_buddy/gym_buddy/NotificationReminder.java
+++ Gym_Buddy2/app/src/main/java/com/dscudr/gym_buddy/gym_buddy/NotificationReminder.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
+import com.dscudr.gym_buddy.gym_buddy.activity.MainActivity;
 
 public class NotificationReminder extends BroadcastReceiver {
 
@@ -20,12 +21,11 @@ public class NotificationReminder extends BroadcastReceiver {
         noti.setContentText("It's Time to Get Hidrated");
         noti.setContentTitle("Drink Water");
         noti.setSound(sound);
-        noti.setSmallIcon(R.mipmap.ic_launcher);
+        noti.setSmallIcon(R.mipmap.gb_logo);
         Intent i = new Intent(context, MainActivity.class);
         PendingIntent pd = PendingIntent.getActivity(context, 1, i, 0);
         noti.setContentIntent(pd);
         noti.setAutoCancel(true);
         managerCompat.notify(1, noti.build());
-        Toast.makeText(context,"inside reciver Gymbuddy ",Toast.LENGTH_SHORT).show();
     }
 }
